d3e88da2126aa58860d1d5c1d41aefa6749f434c
zombarino <andrewkorniychukmining@gmail.com>
2019-08-11 14:21:56 +0200

    new request

---
+++ www/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.social-ricardo').on('click', function(){
+        $('.ricardo').fadeIn(300);
+        $(this).hide();
+    });
+    $('.social-alberto').on('click', function(){
+        $('.alberto').fadeIn(300);
+        $(this).hide();
+    });
+    $('.social-thomas').on('click', function(){
+        $('.thomas').fadeIn(300);
+        $(this).hide();
+    });
+});
